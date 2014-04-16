42d250813e1e25a39b90a82c13e28510d0299c64
Sergio Almecija Rodriguez <sheniff@yahoo-inc.com>
2014-04-16 10:04:03 -0700

    All front end app in Angular

--- angular/app/scripts/controllers/main.js
+++ angular/app/scripts/controllers/main.js
@@ -3,11 +3,43 @@
 angular.module('soyApp')
   .controller('MainCtrl', function ($scope, Post) {
 
-    $scope.stuff = Post.query();
+    // Attributes
 
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.posts = Post.query();
+
+    $scope.newPost = new Post({ lang: 'javascript' });
+
+    // Actions
+
+    $scope.remove = function DeletePost(post) {
+      post.$delete();
+    };
+
+    $scope.create = function CreatePost() {
+      $scope.newPost.$save(
+        function success(data) {
+          $scope.posts.push(data);
+          $scope.newPost = new Post();
+        },
+        function error (data) {
+          console.error('SAVE ERROR: ', data);
+        }
+      )
+    };
+
+    $scope.startNewPost = function StartNewPost() {
+      $scope.creatingPost = true;
+    };
+
+    $scope.stopNewPost = function StopNewPost() {
+      $scope.creatingPost = false;
+    };
+
+    $scope.toggleNewPost = function ToggleNewPost() {
+      if ($scope.creatingPost) {
+        $scope.stopNewPost();
+      } else {
+        $scope.startNewPost();
+      }
+    };
   });
